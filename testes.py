58d4dddd0b3f730638febc11994126cc5fa153ab
derekfq <quaresmaderek@gmail.com>
2023-09-05 16:45:45 -0300

    numpy test

--- testes.py
+++ testes.py
@@ -1,9 +1,23 @@
 # Arquivo de testes
-
+"""
 import numpy as np
 import matplotlib.pyplot as plot
 
-a = np.asarray([1, 2, 3])
+a = np.asarray([[1, 2, 3]]).T
+
 b = np.asarray([4, 5, 6])
 
-print(a+b)
+
+print(len(a))
+print(a.shape)
+
+"""
+import RobPy as rp
+
+a = rp.cria_vetor3([1, 2, 3])
+b = rp.cria_vetor3([4, 5, 6])
+
+print(rp.produto_escalar(a, b))
+
+print(a.shape)
+print(type(a))
